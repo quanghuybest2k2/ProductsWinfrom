62266f99b28611a6e4db9421cb98c66bccc78127
Doan Quang Huy <quanghuybest@gmail.com>
2022-05-16 15:07:28 +0700

    trợ lý ảo

---
+++ Assistant/frmForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Assistant
+{
+    partial class frmForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLiveSpeech = new System.Windows.Forms.Label();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLiveSpeech
+            // 
+            this.lblLiveSpeech.AutoSize = true;
+            this.lblLiveSpeech.Location = new System.Drawing.Point(91, 62);
+            this.lblLiveSpeech.Name = "lblLiveSpeech";
+            this.lblLiveSpeech.Size = new System.Drawing.Size(67, 13);
+            this.lblLiveSpeech.TabIndex = 0;
+            this.lblLiveSpeech.Text = "Live Speech";
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Location = new System.Drawing.Point(94, 108);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(44, 13);
+            this.lblKetQua.TabIndex = 1;
+            this.lblKetQua.Text = "Kết quả";
+            // 
+            // frmForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.lblLiveSpeech);
+            this.Name = "frmForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLiveSpeech;
+        private System.Windows.Forms.Label lblKetQua;
+    }
+}
+
---
+++ Assistant/frmForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace Assistant
+{
+    public partial class frmForm : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer speech = new SpeechSynthesizer();
+        public frmForm()
+        {
+            InitializeComponent();
+            Choices choices = new Choices();
+            string[] text = File.ReadAllLines(Environment.CurrentDirectory + "//grammer.txt");
+            choices.Add(text);
+            Grammar grammer = new Grammar(new GrammarBuilder(choices));
+            recEngine.LoadGrammar(grammer);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            recEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recEngine_SpeechRecognized);
+
+            speech.SelectVoiceByHints(VoiceGender.Female);
+
+        }
+
+        private void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string result = e.Result.Text;
+            //if (result == "Hello")
+            //{
+            //    result = "Hello, Can I help you?";
+            //}
+            string open = "Ok, I open ";
+            switch (result)
+            {
+                case "Hello":
+                    result = "Hello, Can I help you?";
+                    break;
+                case "What time is it?":
+                    result = "It's currently " + DateTime.Now.ToLongTimeString();
+                    break;
+                case "Open Google Chrome":
+                    System.Diagnostics.Process.Start("https://www.google.com/");
+                    result = open + "Google Chrome";
+                    break;
+                case "Open Visual Studio Code":
+                    System.Diagnostics.Process.Start(@"C:\Users\quang\AppData\Local\Programs\Microsoft VS Code\Code.exe");
+                    result = open + "Visual Studio Code";
+                    break;
+                case "Close Visual Studio Code":
+                    System.Diagnostics.Process[] close = System.Diagnostics.Process.GetProcessesByName("Code");
+                    foreach (System.Diagnostics.Process p in close)
+                        p.Kill();
+                    result = "Closing Visual Studio Code";
+                    break;
+                case "Goodbye":
+                    Application.Exit();
+                    break;
+            }
+            speech.SpeakAsync(result);
+            lblKetQua.Text = result;
+        }
+    }
+}
