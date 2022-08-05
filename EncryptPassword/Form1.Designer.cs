116aaa0316f82b4a3d788c4598deabdec0101b5b
Doan Quang Huy <quanghuybest@gmail.com>
2022-08-05 20:31:17 +0700

    Mã hóa mật khẩu

---
+++ EncryptPassword/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace EncryptPassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lable2 = new System.Windows.Forms.Label();
+            this.txtMaHoa = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.btnMaHoa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mật khẩu";
+            // 
+            // lable2
+            // 
+            this.lable2.AutoSize = true;
+            this.lable2.Location = new System.Drawing.Point(28, 105);
+            this.lable2.Name = "lable2";
+            this.lable2.Size = new System.Drawing.Size(90, 13);
+            this.lable2.TabIndex = 0;
+            this.lable2.Text = "Mật khẩu mã hóa";
+            // 
+            // txtMaHoa
+            // 
+            this.txtMaHoa.Location = new System.Drawing.Point(156, 102);
+            this.txtMaHoa.Name = "txtMaHoa";
+            this.txtMaHoa.Size = new System.Drawing.Size(252, 20);
+            this.txtMaHoa.TabIndex = 1;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(156, 45);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(252, 20);
+            this.txtMatKhau.TabIndex = 1;
+            // 
+            // btnMaHoa
+            // 
+            this.btnMaHoa.Location = new System.Drawing.Point(262, 135);
+            this.btnMaHoa.Name = "btnMaHoa";
+            this.btnMaHoa.Size = new System.Drawing.Size(116, 23);
+            this.btnMaHoa.TabIndex = 2;
+            this.btnMaHoa.Text = "Mã hóa";
+            this.btnMaHoa.UseVisualStyleBackColor = true;
+            this.btnMaHoa.Click += new System.EventHandler(this.btnMaHoa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 170);
+            this.Controls.Add(this.btnMaHoa);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtMaHoa);
+            this.Controls.Add(this.lable2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lable2;
+        private System.Windows.Forms.TextBox txtMaHoa;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Button btnMaHoa;
+    }
+}
+
---
+++ EncryptPassword/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace EncryptPassword
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string Encrypt(string value)
+        {
+            //Using MD5 to encrypt a string
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                UTF8Encoding utf8 = new UTF8Encoding();
+                //Hash data
+                byte[] data = md5.ComputeHash(utf8.GetBytes(value));
+                return Convert.ToBase64String(data);
+            }
+        }
+
+        private void btnMaHoa_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtMatKhau.Text))
+            {
+                MessageBox.Show("Vui lòng nhập mật khẩu.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            txtMaHoa.Text = Encrypt(txtMatKhau.Text);
+        }
+    }
+}
