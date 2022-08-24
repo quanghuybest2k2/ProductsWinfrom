0ee37c368f8997174129df04b604dc7b80c15800
Doan Quang Huy <quanghuybest@gmail.com>
2022-08-24 20:53:15 +0700

    count down

---
+++ CountDown/frmMain.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace CountDown
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbbPhut = new System.Windows.Forms.ComboBox();
+            this.cbbGiay = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timerCountDown = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // cbbPhut
+            // 
+            this.cbbPhut.FormattingEnabled = true;
+            this.cbbPhut.Location = new System.Drawing.Point(42, 53);
+            this.cbbPhut.Name = "cbbPhut";
+            this.cbbPhut.Size = new System.Drawing.Size(132, 21);
+            this.cbbPhut.TabIndex = 0;
+            // 
+            // cbbGiay
+            // 
+            this.cbbGiay.FormattingEnabled = true;
+            this.cbbGiay.Location = new System.Drawing.Point(220, 53);
+            this.cbbGiay.Name = "cbbGiay";
+            this.cbbGiay.Size = new System.Drawing.Size(132, 21);
+            this.cbbGiay.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Phút";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(259, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Giây";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(58, 216);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(116, 46);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(236, 216);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(116, 46);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(102, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 73);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "00:00";
+            // 
+            // timerCountDown
+            // 
+            this.timerCountDown.Interval = 1000;
+            this.timerCountDown.Tick += new System.EventHandler(this.timerCountDown_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 302);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbbGiay);
+            this.Controls.Add(this.cbbPhut);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đếm ngược thời gian";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbbPhut;
+        private System.Windows.Forms.ComboBox cbbGiay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timerCountDown;
+    }
+}
+
---
+++ CountDown/frmMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountDown
+{
+    public partial class frmMain : Form
+    {
+        private int toTalSecond;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.btnStop.Enabled = false;
+            for (int i = 0; i < 60; i++)
+            {
+                this.cbbPhut.Items.Add(i.ToString());
+                this.cbbGiay.Items.Add(i.ToString());
+            }
+            this.cbbPhut.SelectedIndex = 30;
+            this.cbbGiay.SelectedIndex = 59;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            this.btnStart.Enabled = false;
+            this.btnStop.Enabled = true;
+
+            int m = int.Parse(this.cbbPhut.SelectedItem.ToString());
+            int s = int.Parse(this.cbbGiay.SelectedItem.ToString());
+
+            toTalSecond = (m * 60) + s;
+            this.timerCountDown.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            this.btnStart.Enabled = true;
+            this.btnStop.Enabled = false;
+        }
+
+        private void timerCountDown_Tick(object sender, EventArgs e)
+        {
+            if (toTalSecond>0)
+            {
+                toTalSecond--;
+                int m = toTalSecond / 60;
+                int s = toTalSecond - (m * 60);
+                this.label3.Text = m.ToString() + ":" + s.ToString();
+            }
+            else
+            {
+                this.timerCountDown.Stop();
+                MessageBox.Show("Hết giờ làm rồi!");
+            }
+        }
+    }
+}
