487fb610a8e957cca27a0271084a3e625ab49199
Doan Quang Huy <quanghuybest@gmail.com>
2022-09-02 11:32:44 +0700

    thông báo nhắc nhở

--- HienThiThoiGian/Form1.Designer.cs
+++ HienThiThoiGian/Form1.Designer.cs
@@ -30,53 +30,112 @@ namespace HienThiThoiGian
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.lb_thoigian = new System.Windows.Forms.Label();
-            this.lb_ngay = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblGio = new System.Windows.Forms.Label();
+            this.lb_ThoiGian = new System.Windows.Forms.Label();
+            this.notifyThongBao = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ctxmNotify = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblPhut = new System.Windows.Forms.Label();
+            this.timerNhacNho = new System.Windows.Forms.Timer(this.components);
+            this.timerThoiGian = new System.Windows.Forms.Timer(this.components);
+            this.ctxmNotify.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lb_thoigian
+            // lblGio
             // 
-            this.lb_thoigian.AutoSize = true;
-            this.lb_thoigian.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.lb_thoigian.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_thoigian.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lb_thoigian.Location = new System.Drawing.Point(71, 68);
-            this.lb_thoigian.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.lb_thoigian.Name = "lb_thoigian";
-            this.lb_thoigian.Size = new System.Drawing.Size(169, 19);
-            this.lb_thoigian.TabIndex = 0;
-            this.lb_thoigian.Text = "Thời gian hiện tại";
+            this.lblGio.AutoSize = true;
+            this.lblGio.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblGio.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGio.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblGio.Location = new System.Drawing.Point(60, 68);
+            this.lblGio.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblGio.Name = "lblGio";
+            this.lblGio.Size = new System.Drawing.Size(31, 19);
+            this.lblGio.TabIndex = 0;
+            this.lblGio.Text = "00";
             // 
-            // lb_ngay
+            // lb_ThoiGian
             // 
-            this.lb_ngay.AutoSize = true;
-            this.lb_ngay.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.lb_ngay.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_ngay.Location = new System.Drawing.Point(71, 182);
-            this.lb_ngay.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.lb_ngay.Name = "lb_ngay";
-            this.lb_ngay.Size = new System.Drawing.Size(147, 19);
-            this.lb_ngay.TabIndex = 1;
-            this.lb_ngay.Text = "Ngày hôm nay";
+            this.lb_ThoiGian.AutoSize = true;
+            this.lb_ThoiGian.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lb_ThoiGian.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_ThoiGian.Location = new System.Drawing.Point(71, 182);
+            this.lb_ThoiGian.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lb_ThoiGian.Name = "lb_ThoiGian";
+            this.lb_ThoiGian.Size = new System.Drawing.Size(57, 19);
+            this.lb_ThoiGian.TabIndex = 1;
+            this.lb_ThoiGian.Text = "Hour";
             // 
-            // timer1
+            // notifyThongBao
             // 
-            this.timer1.Enabled = true;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            this.notifyThongBao.ContextMenuStrip = this.ctxmNotify;
+            this.notifyThongBao.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyThongBao.Icon")));
+            this.notifyThongBao.Text = "notifyThongBao";
+            this.notifyThongBao.Visible = true;
+            // 
+            // ctxmNotify
+            // 
+            this.ctxmNotify.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thoátToolStripMenuItem});
+            this.ctxmNotify.Name = "ctxmNotify";
+            this.ctxmNotify.Size = new System.Drawing.Size(105, 26);
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.thoátToolStripMenuItem.Text = "Thoát";
+            this.thoátToolStripMenuItem.Click += new System.EventHandler(this.thoátToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Thông báo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblPhut
+            // 
+            this.lblPhut.AutoSize = true;
+            this.lblPhut.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblPhut.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhut.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblPhut.Location = new System.Drawing.Point(103, 68);
+            this.lblPhut.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblPhut.Name = "lblPhut";
+            this.lblPhut.Size = new System.Drawing.Size(31, 19);
+            this.lblPhut.TabIndex = 0;
+            this.lblPhut.Text = "00";
+            // 
+            // timerNhacNho
+            // 
+            this.timerNhacNho.Interval = 1000;
+            this.timerNhacNho.Tick += new System.EventHandler(this.timerNhacNho_Tick);
+            // 
+            // timerThoiGian
+            // 
+            this.timerThoiGian.Tick += new System.EventHandler(this.timerThoiGian_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(475, 337);
-            this.Controls.Add(this.lb_ngay);
-            this.Controls.Add(this.lb_thoigian);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb_ThoiGian);
+            this.Controls.Add(this.lblPhut);
+            this.Controls.Add(this.lblGio);
             this.Font = new System.Drawing.Font("Rockwell Extra Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
             this.Name = "Form1";
             this.Text = "Hệ thống ngày giờ";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.ctxmNotify.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,9 +143,15 @@ namespace HienThiThoiGian
 
         #endregion
 
-        private System.Windows.Forms.Label lb_thoigian;
-        private System.Windows.Forms.Label lb_ngay;
-        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblGio;
+        private System.Windows.Forms.Label lb_ThoiGian;
+        private System.Windows.Forms.NotifyIcon notifyThongBao;
+        private System.Windows.Forms.ContextMenuStrip ctxmNotify;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblPhut;
+        private System.Windows.Forms.Timer timerNhacNho;
+        private System.Windows.Forms.Timer timerThoiGian;
     }
 }
 
--- HienThiThoiGian/Form1.cs
+++ HienThiThoiGian/Form1.cs
@@ -12,6 +12,11 @@ namespace HienThiThoiGian
 {
     public partial class Form1 : Form
     {
+        //lb_thoigian.Text = DateTime.Now.ToLongTimeString();
+        string _gio;
+        string _phut;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -19,15 +24,40 @@ namespace HienThiThoiGian
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            timer1.Start();
-            lb_thoigian.Text = DateTime.Now.ToLongTimeString();
-            lb_ngay.Text = DateTime.Now.ToLongDateString();
+            timerThoiGian.Start();
+            timerNhacNho.Start();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            lb_thoigian.Text = DateTime.Now.ToLongTimeString();
-            timer1.Start();
+
+        }
+
+        private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyThongBao.Dispose();
+        }
+
+        private void timerNhacNho_Tick(object sender, EventArgs e)
+        {
+            switch (_gio)
+            {
+                case "11" when (_phut == "31"):
+                    notifyThongBao.ShowBalloonTip(1000, "Nhắc nhở học bài", "Bạn ơi! mới đó mà đã nản rồi ư? Sắp thành công rồi mà", ToolTipIcon.None);
+                    timerNhacNho.Stop();// hiển thị 1 lần rồi dừng, tránh lặp
+                    break;
+                case "21" when (_phut == "00"):
+                    notifyThongBao.ShowBalloonTip(1000, "Nhắc nhở đi ngủ", "Hi hi bạn nên đi ngủ đi", ToolTipIcon.None);
+                    timerNhacNho.Stop();// hiển thị 1 lần rồi dừng, tránh lặp
+                    break;
+            }
+        }
+
+        private void timerThoiGian_Tick(object sender, EventArgs e)
+        {
+            lb_ThoiGian.Text = DateTime.Now.ToString("HH:mm:s");
+            _gio = DateTime.Now.Hour.ToString();
+            _phut = DateTime.Now.Minute.ToString();
         }
     }
 }
